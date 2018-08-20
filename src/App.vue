6875f8c8a3fee3daca9224e9402d1f10eb484494
malun <malun666@126.com>
2018-08-20 11:31:54 +0800

    added the element

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,42 @@
 <template>
   <div id="app">
-    <p>这里是app的内容</p>
-    <p>{{age}}</p>
-    <hr>
-    <demo></demo>
+    <img src="./assets/logo.png">
+    <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button>el-button</el-button>
+
+      <hr>
+      <div class="block">
+        <span class="demonstration">默认</span>
+        <el-slider v-model="value1"></el-slider>
+      </div>
+    </div>
+    <div class="block">
+      <span class="demonstration">自定义初始值</span>
+      <el-slider v-model="value2"></el-slider>
+    </div>
+    <div class="block">
+      <span class="demonstration">隐藏 Tooltip</span>
+      <el-slider v-model="value3" :show-tooltip="false"></el-slider>
+    </div>
+    <div class="block">
+      <span class="demonstration">格式化 Tooltip</span>
+      <el-slider v-model="value4" :format-tooltip="formatTooltip"></el-slider>
+    </div>
+    <div class="block">
+      <span class="demonstration">禁用</span>
+      <el-slider v-model="value5" disabled></el-slider>
+    </div>
   </div>
 </template>
 
 <script>
 import Demo from "./components/Demo";
+import "element-ui/lib/theme-chalk/index.css";
 
 export default {
   name: "app",
---
+++ src/plugins/element.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import { Button, Slider } from 'element-ui'
+
+Vue.use(Button); // 就是把Button按钮注册成全局组件
+// Vue.component('slide', Slider)
+Vue.use(Slider);
