052a517f4b67f2fecfafdacb87059775af38d0da
malun <malun666@126.com>
2018-08-20 11:56:05 +0800

    added the axios

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,49 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-
-      <hr>
-      <div class="block">
-        <span class="demonstration">默认</span>
-        <el-slider v-model="value1"></el-slider>
-      </div>
-    </div>
-    <div class="block">
-      <span class="demonstration">自定义初始值</span>
-      <el-slider v-model="value2"></el-slider>
-    </div>
-    <div class="block">
-      <span class="demonstration">隐藏 Tooltip</span>
-      <el-slider v-model="value3" :show-tooltip="false"></el-slider>
-    </div>
-    <div class="block">
-      <span class="demonstration">格式化 Tooltip</span>
-      <el-slider v-model="value4" :format-tooltip="formatTooltip"></el-slider>
-    </div>
-    <div class="block">
-      <span class="demonstration">禁用</span>
-      <el-slider v-model="value5" disabled></el-slider>
-    </div>
+    <h1>用户列表</h1>
+    <table class="table is-striped is-bordered">
+      <thead>
+        <tr>
+          <th>编号</th>
+          <th>姓名</th>
+          <th>电话</th>
+          <th>地址</th>
+          <th>编辑</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) of userList" :key="index">
+          <td>{{item.id}}</td>
+          <td>{{item.name}}</td>
+          <td>{{item.phone}}</td>
+          <td>{{item.address}}</td>
+          <td><a href="javascript:">删除</a></td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 import Demo from "./components/Demo";
 import "element-ui/lib/theme-chalk/index.css";
+import axios from "axios"; // ajax
 
 export default {
   name: "app",
   data() {
     return {
-      age: 19
+      age: 19,
+      userList: []
     };
+  },
+  created() {
+    axios.get("http://localhost:12345/users").then(res => {
+      this.userList = res.data;
+      // this.userList.push(...res.data);
+    });
+
+    // .catch(e => {});
   },
   components: {
     demo: Demo
